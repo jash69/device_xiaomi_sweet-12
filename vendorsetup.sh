dafea7595178b2ceda27b04f2ec593e4434566d8
zaidkhan0997 <kzaid0997@gmail.com>
2022-01-26 14:53:10 +0530

    sweet: track dolby repo from gitlab
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,4 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone -b arrow-12.0-caf-sm8150 https://github.com/ArrowOS/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
+git clone -b twelve https://gitlab.com/sweetin/android-vendor-dolby.git vendor/dolby
