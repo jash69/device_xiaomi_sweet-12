a99e68c5d896a6360f8b1c2074d467a8950acea9
jash <jashvakharia@gmail.com>
2022-03-06 13:51:12 +0530

    sweet: Build SuperiorOS

--- superior_sweet.mk
+++ superior_sweet.mk
@@ -5,16 +5,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Radiant configurations
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit common SuperiorOS configurations
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Radiant Stuff
-#RADIANT_BUILD_TYPE := OFFICIAL
-DERP_BUILDTYPE := Official
-PRODUCT_NAME := derp_sweet
+# SuperiorOS Stuff
+PRODUCT_NAME := superior_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
